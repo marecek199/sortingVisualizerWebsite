f444dbfefbb387348c3c8dd8fe9ab52cd79e0d34
xsoosm00 <94903451+xsoosm00@users.noreply.github.com>
2022-01-04 16:28:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,9 +45,11 @@ app.get('/snake/:id', (req, res) => {
 })
 
 
-app.listen(8080, (req, res) => {
-    console.log('Server is running on port 8080!')
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port${ PORT }`);
+});
+
 
 
 
